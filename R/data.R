fc125bbd2a47f337cb2be4632a5702cedc4d9d42
owen-hy <owenhy@umich.edu>
2025-01-26 16:31:26 -0500

    Added data/ and example dataset with documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @title Example heart rate data set for one subject
+#'
+#' @description FitBit heart rate measurement from one subject
+#'
+#'
+#'@format A data frame with 154104 rows and 3 variables:
+#'\describe{
+#'\item{id}{id, as provided by Kaggle}
+#'\item{time}{time of observation, in format %Y-%m-%d %H:%M:%S}
+#'\item{hr}{heart rate, measured in BPM}
+#'}
+#'@source \url{https://www.kaggle.com/datasets/arashnic/fitbit/versions/1}
+#'
+"example_heart_1"
