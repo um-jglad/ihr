5323642333a49a69c15182e75b8224d0c75465b0
sophihan <157166794+sophihan@users.noreply.github.com>
2025-01-29 12:25:17 -0500

    Update active_percent.R documentation

--- R/active_percent.R
+++ R/active_percent.R
@@ -1,3 +1,23 @@
+#' Calculate the % missingness of the heart rate value
+
+#' @Description
+#' The function active_percent computes the percentage missingness of the heart rate data.
+
+#' @Usage
+#' active_percent(data)
+
+#' @para
+#' data: A DataFrame object with column names "Id", "Time", "Value". 
+#' Missing HR values (NA) are automatically excluded from calculations.
+
+#' @return
+#' If a dataframe object is passed, then a tibble object with a column for subject id and a column for each of percentage missingness is returned.
+#' 'NA' heartrate values are omitted from the calcution of the summary values.
+
+#' @examples
+#' data(example_heart_1)
+#' active_percent(example_heart_1)
+
 active_percent <- function(data) {
   # Ensure necessary columns exist
   if (!all(c("Id", "Time") %in% colnames(data))) {
@@ -8,6 +28,9 @@ active_percent <- function(data) {
   data <- data %>%
     mutate(Time = as.POSIXct(Time, format = "%m/%d/%Y %I:%M:%S %p"),
            Time_minute = floor_date(Time, "minute")) 
+  
+  # Remove rows with missing HR values
+  data <- dplyr::filter(data, !is.na(Value))
   
   # Group by Id and compute active percentage and measurement period length
   active_data <- data %>%
