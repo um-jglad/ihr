0a338bebcd36b5c39407539de0567be483986315
Irina Gaynanova <irina.gain@gmail.com>
2025-01-29 16:18:48 -0500

    added export to summary functions

--- R/active_percent.R
+++ R/active_percent.R
@@ -7,13 +7,14 @@
 #' active_percent(data)
 
 #' @para
-#' data: A DataFrame object with column names "Id", "Time", "Value". 
+#' data: A DataFrame object with column names "Id", "Time", "Value".
 #' Missing HR values (NA) are automatically excluded from calculations.
 
 #' @return
 #' If a dataframe object is passed, then a tibble object with a column for subject id and a column for each of percentage missingness is returned.
 #' 'NA' heartrate values are omitted from the calcution of the summary values.
 
+#' @export
 #' @examples
 #' data(example_heart_1)
 #' active_percent(example_heart_1)
@@ -23,23 +24,23 @@ active_percent <- function(data) {
   if (!all(c("Id", "Time") %in% colnames(data))) {
     stop("The dataset must contain 'Id' and 'Time' columns.")
   }
-  
+
   # Convert Time column to Date-Time format
   data <- data %>%
     mutate(Time = as.POSIXct(Time, format = "%m/%d/%Y %I:%M:%S %p"),
-           Time_minute = floor_date(Time, "minute")) 
-  
+           Time_minute = floor_date(Time, "minute"))
+
   # Remove rows with missing HR values
   data <- dplyr::filter(data, !is.na(Value))
-  
+
   # Group by Id and compute active percentage and measurement period length
   active_data <- data %>%
     group_by(Id) %>%
     summarize(
-      total_minutes = as.numeric(difftime(max(Time), min(Time), units = "mins")), 
-      unique_minutes = n_distinct(Time_minute),  
-      active_percent = (unique_minutes / total_minutes) * 100  
+      total_minutes = as.numeric(difftime(max(Time), min(Time), units = "mins")),
+      unique_minutes = n_distinct(Time_minute),
+      active_percent = (unique_minutes / total_minutes) * 100
     )
-  
+
   return(active_data)
 }
--- R/summary_hr.R
+++ R/summary_hr.R
@@ -1,7 +1,7 @@
 #' Calculate summary heart rate
 
 #' @Description
-#' The function summary_hr computes summary statistics for heart rate (HR) data. 
+#' The function summary_hr computes summary statistics for heart rate (HR) data.
 #' It is a wrapper for summary() functions and outputs a tibble object with the subject ID and the following summary values:
 #' Minimum, 1st Quartile, Median, Mean, 3rd Quartile, Maximum, Standard Deviation (SD), Total number of unique days.
 
@@ -9,13 +9,14 @@
 #' summary_hr(data)
 
 #' @para
-#' data: A DataFrame object with column names "Id", "Time", "Value". 
+#' data: A DataFrame object with column names "Id", "Time", "Value".
 #' Missing HR values (NA) are automatically excluded from calculations.
 
 #' @return
 #' If a dataframe object is passed, then a tibble object with a column for subject id and a column for each of summary values is returned.
 #' 'NA' heartrate values are omitted from the calcution of the summary values.
 
+#' @export
 #' @examples
 #' data(example_heart_1)
 #' summary_hr(example_heart_1)
@@ -25,14 +26,14 @@ summary_hr <- function(data) {
   if (!all(c("Id", "Value") %in% colnames(data))) {
     stop("The dataset must contain 'Id' and 'Value' columns.")
   }
-  
+
   # Convert Time column to Date-Time format
   data <- data %>%
     mutate(Time = as.POSIXct(Time, format = "%m/%d/%Y %I:%M:%S %p"))
-  
+
   # Remove rows with missing HR values
   data <- dplyr::filter(data, !is.na(Value))
-  
+
   # Group by ID and compute summary statistics for HR
   summary_data <- data %>%
     dplyr::group_by(Id) %>%
@@ -46,6 +47,6 @@ summary_hr <- function(data) {
       sd_hr = sd(Value, na.rm = TRUE),
       total_days = length(unique(format(Time, "%Y-%m-%d")))
     )
-  
+
   return(summary_data)
 }
