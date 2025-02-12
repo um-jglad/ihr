af63dbc23588df0c69d69e4d85728a57b90cc41b
sophihan <157166794+sophihan@users.noreply.github.com>
2025-02-12 12:33:45 -0500

    Update data.R

--- R/data.R
+++ R/data.R
@@ -12,3 +12,19 @@
 #'@source \url{https://www.kaggle.com/datasets/arashnic/fitbit/versions/1}
 #'
 "example_heart_1"
+
+
+#' @title Example heart rate data set for five subjects
+#'
+#' @description FitBit heart rate measurement from five subjects
+#'
+#'
+#'@format A data frame with 530859 rows and 3 variables:
+#'\describe{
+#'\item{id}{id, as provided by Kaggle}
+#'\item{time}{time of observation, in format %Y-%m-%d %H:%M:%S}
+#'\item{hr}{heart rate, measured in BPM}
+#'}
+#'@source \url{https://www.kaggle.com/datasets/arashnic/fitbit/versions/1}
+#'
+"example_heart_5"
