9605979669213a8251d460b0334b9fa330b04b0d
Sophia Han <sophihan@umich.edu>
2025-03-11 11:49:28 -0400

    add documentation to the calculate_RHR

--- R/calculate_RHR.R
+++ R/calculate_RHR.R
@@ -1,3 +1,27 @@
+#' Calculate resting heart rate
+
+#' @description
+#' The function calculate_RHR computes the resting heart rate.
+
+#' @usage
+#' calculate_RHR(data)
+
+#' @param data A DataFrame object with column names "id", "time", "hr".
+
+#' @return
+#' If a dataframe object is passed, then a tibble object with a column for subject id and a column for each of summary values is returned.
+#' 'NA' heartrate values are omitted from the calculation of the summary values.
+
+#' @references
+#' Measure by measure: Resting heart rate across the 24-hour cycle,
+#' \doi{10.1371/journal.pdig.0000236}.
+
+#' @export
+#' @examples
+#' data(example_heart_1)
+#' summary_hr(example_heart_1)
+
+
 calculate_RHR <- function(data) {
   data$time <- as.POSIXct(data$time, format="%Y-%m-%d %H:%M:%S")
   data$hour <- format(data$time, "%H")
