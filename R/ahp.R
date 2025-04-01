5360619d3c16db523bd44824145fd51c6d20a9a5
Sophia Han <sophihan@umich.edu>
2025-04-01 14:31:18 -0400

    add ahp.R

---
+++ R/ahp.R
@@ -0,0 +1,65 @@
+#' Display Ambulatory Heart rate Profile (AHP) statistics for selected subject
+#'
+#' @usage
+#' agp(data, maxd = 14, inter_gap = 45, dt0 = NULL, tz = "", daily = TRUE)
+#'
+#' @inheritParams HR2DayByDay
+#' @param daily \strong{Default: TRUE.} Logical indicator whether AHP should include separate daily plots.
+#'
+#' @return A plot displaying heart rate measurements range, selected heart rate statistics (Resting Heart Rate, Heart Rate Reserve), percentage spent in target ranges and quantiles of 24 hour profile.
+#'
+#' @export
+#'
+#' @examples
+#' data(example_heart_1)
+#' agp(example_heart_1, daily = FALSE)
+
+
+ahp <- function(data, maxd = 14, inter_gap = 45, dt0 = NULL, tz = "", daily = TRUE){
+
+  id = NULL
+  rm(list = c("id"))
+
+  subject = unique(data$id)
+  ns = length(subject)
+  if (ns > 1){
+    subject = subject[1]
+    warning(paste("The provided data have", ns, "subjects. The plot will only be created for subject", subject))
+    data = data %>% dplyr::filter(id == subject)
+  }
+
+  # Calculate range of measurements
+  out_range = percent_nonmissing(data)
+
+  RHR_data <- calculate_RHR(data)
+  HRR_data <- calculate_HRR(data)
+
+  # Extract values for subject
+  RHR_value <- if (!is.null(RHR_data)) round(RHR_data$RHR[RHR_data$id == subject]) else NA
+  HRR_value <- if (!is.null(HRR_data)) round(HRR_data$HRR[HRR_data$id == subject]) else NA
+
+  # Calculate metrics
+  tableStat = data.frame("Heartrate statistics", "value")
+  tableStat[1, 1] = "id"
+  tableStat[1, 2] = as.character(subject)
+  tableStat[2, 1] = "Start Date"
+  tableStat[2, 2] = as.character(out_range$start_date)
+  tableStat[3, 1] = "End Date"
+  tableStat[3, 2] = as.character(out_range$end_date)
+  tableStat[4, 1] = "Duration"
+  tableStat[4, 2] = paste(as.character(out_range$unique_days),"days")
+  tableStat[5, 1] = "% Time Heart rate is Active"
+  tableStat[5, 2] = paste0(round(out_range$active_percent), "%")
+  tableStat[6, 1] = "Resting Heart rate"
+  tableStat[6, 2] = if (!is.na(RHR_value)) paste0(RHR_value, " bpm") else "Not available"
+  tableStat[7, 1] = "Heart rate reserve"
+  tableStat[7, 2] = if (!is.na(HRR_value)) paste0(HRR_value, " bpm") else "Not available"
+
+
+  # Make a pretty table
+  mytheme <- gridExtra::ttheme_default(core = list(fg_params = list(hjust = 0, x = 0.03, fontsize = 12)), colhead = list(fg_params = list(fontsize=9, fontface="bold")))
+
+  t1 = gridExtra::tableGrob(tableStat, rows = NULL, cols = NULL, theme = mytheme)
+  pFinal = (patchwork::wrap_elements(t1))
+  pFinal
+}
