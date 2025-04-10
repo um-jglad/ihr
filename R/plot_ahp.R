c064807a0b39dc62e684abc7e2e448ecff97eb76
owen-hy <owenhy@umich.edu>
2025-04-10 10:21:35 -0400

    Documentation and Build fixes

--- R/plot_ahp.R
+++ R/plot_ahp.R
@@ -1,5 +1,31 @@
-plot_ahp <- function (data, LLTR = 70, ULTR = 180, smooth = TRUE,
-                      span = 0.3, dt0 = 1, inter_gap = 15, tz = "", title = FALSE) {
+#' Plot Ambulatory Heart Rate Profile (AHP) modal day
+#'
+#' @description
+#' The function plot_ahp produces an AhP plot that collapses all data into a single 24 hr "modal day".
+#'
+#' @usage
+#' plot_ahp(data, smooth = FALSE, span = 0.3,
+#' inter_gap = 15, tz = "", title = FALSE)
+#'
+#' @param data DataFrame object with column names "id", "time", "hr"
+#' @param inter_gap The maximum allowable gap (in minutes) for interpolation. The values will not be interpolated between the Heart Rate measurements that are more than inter_gap minutes apart.
+#' @param tz  A character string specifying the time zone to be used. System-specific (see \code{\link{as.POSIXct}}), but " " is the current time zone, and "GMT" is UTC (Universal Time, Coordinated). Invalid values are most commonly treated as UTC, on some platforms with a warning
+#' @param title Indicator whether the title of the plot should display the subject ID. The default is FALSE (no title).
+#' @param smooth Boolean indicating whether quantiles should be smoothed before plotting, default is TRUE
+#' @param span Optional parameter indicating span for loess smoothing. Default is 0.3, larger values result in more smoothing,
+#' recommended to choose between 0.1 to 0.7.
+#'
+#' @return Plot of a 24 hr modal day collapsing all data to a single day.
+#'
+#' @export
+#'
+#' @examples
+#' data(example_heart_1)
+#' plot_ahp(example_heart_1)
+#'
+
+plot_ahp <- function (data, smooth = FALSE,
+                      span = 0.3, inter_gap = 15, tz = "", title = FALSE) {
 
   # Summarizing data at a minute level
   data <- data |>
@@ -15,7 +41,7 @@ plot_ahp <- function (data, LLTR = 70, ULTR = 180, smooth = TRUE,
   if (ns > 1){
     subject = subject[1]
     warning(paste("The provided data have", ns, "subjects. The plot will only be created for subject", subject))
-    data = data %>% dplyr::filter(id == subject)
+    data = data |> dplyr::filter(id == subject)
   }
 
   data_ip = HR2DayByDay(data, dt0 = 1, inter_gap = inter_gap, tz = tz)
@@ -52,8 +78,6 @@ plot_ahp <- function (data, LLTR = 70, ULTR = 180, smooth = TRUE,
                          fill = "#A7BEE7", alpha = 0.5) +
     ggplot2::geom_ribbon(ggplot2::aes(times, ymin = twentyfive, ymax = seventyfive),
                          fill = "#325DAA", alpha = 0.5) +
-    ggplot2::geom_hline(yintercept = LLTR, color = '#48BA3C') +
-    ggplot2::geom_hline(yintercept = ULTR, color = '#48BA3C') +
     ggplot2::scale_x_time(breaks = c(hms::as_hms(c('00:00:00', '03:00:00', '06:00:00', '09:00:00', '12:00:00',
                                                    '15:00:00', '18:00:00', '21:00:00', '24:00:00'))),
                           labels = c('12 am', '3 am', '6 am', '9 am', '12 pm',
