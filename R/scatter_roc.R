d7717d7b7260ab52dd243032fbd5c7c88a3fbc46
owen-hy <owenhy@umich.edu>
2025-04-15 10:55:32 -0400

    Documentation adds and Personalized thresholds in scatter_roc

--- R/scatter_roc.R
+++ R/scatter_roc.R
@@ -1,3 +1,34 @@
+#' Plot the relationship between Rate of Change (ROC) and Heart Rate
+#'
+#' @description
+#' The function \code{scatter_roc} produces a scatterplot of ROC and Heart Rate
+#'
+#' @usage
+#' scatter_roc(data, timelag = 1, inter_gap = 15, tz = "")
+#'
+#' @inheritParams roc
+#'
+#' @return A ggplot object with ROC on the X-axis and the latter Heart Rate value on the Y-axis
+#'
+#' @export
+#'
+#' @details
+#' A ggplot is returned that shows the relationship of a subject's Rate of Change and the subject's
+#' heart rate. This allows a personalized insight into every subject's ROC, understanding the heart rate in which they ended at
+#' and whether the ROC value comes from a higher activity heart rate or from another context.
+#'
+#' The horizontal lines separate heart rate into 4 distinct sections. These are similar to that generated from
+#' \code{plot_ranges_PA}. The thresholds here are as follows:
+#' 1. Vigorous: >= 60 % HRR + RHR
+#' 2. Moderate: 40 % HRR + RHR -  60 % HRR + RHR
+#' 3. Light: 20 % HRR + RHR -  40 % HRR + RHR
+#' 4. Sedentary/Sleep: <= 20 % HRR + RHR
+
+#'
+#' @examples
+#' data(example_heart_1)
+#' scatter_roc(example_heart_1)
+#'
 scatter_roc <- function(data, timelag = 1, inter_gap = 15, tz = ""){
 
   id = hr = NULL
@@ -27,13 +58,25 @@ scatter_roc <- function(data, timelag = 1, inter_gap = 15, tz = ""){
     dplyr::reframe(
       roc = roc(data.frame(id, time, hr), timelag, inter_gap, tz)$roc)
 
-
   roc_data <- cbind(roc_data, full_hr) |>
     dplyr::filter(!is.na(roc))
 
+  # Generating personalized HR Vals
+
+  personal_hr <- calculate_HRR(data)
+
+  personal_hr <- personal_hr |>
+    mutate(hr_20 = round(RHR + 0.20 * HRR, 1),
+           hr_40 = round(RHR + 0.40 * HRR, 1),
+           hr_60 = round(RHR + 0.60 * HRR, 1))
+
   .p <- roc_data |>
     ggplot(aes(x = roc, y = hr)) +
-    geom_point() +
+    geom_point(alpha = 0.3) +
+    geom_vline(xintercept = 0, color = "red", linetype = "dashed") +
+    geom_hline(data = personal_hr, aes(yintercept = hr_20), color = "#0073C2", linetype = "dashed") +
+    geom_hline(data = personal_hr, aes(yintercept = hr_40), color = "#48BA3C", linetype = "dashed") +
+    geom_hline(data = personal_hr, aes(yintercept = hr_60), color = "#F9B500", linetype = "dashed") +
     facet_wrap(~id) +
     scale_x_continuous(name = "Rate of Change") +
     scale_y_continuous(name = "Heart Rate (BPM)")
@@ -41,5 +84,4 @@ scatter_roc <- function(data, timelag = 1, inter_gap = 15, tz = ""){
   return(.p)
 
 }
-
 
