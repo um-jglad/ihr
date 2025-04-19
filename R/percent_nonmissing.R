bd94493fbf4ccfe3e14b0d935ec66484d69bab3e
Sophia Han <sophihan@umich.edu>
2025-04-19 19:55:28 -0400

    update the percent_nonmissing to a minute level

--- R/percent_nonmissing.R
+++ R/percent_nonmissing.R
@@ -19,8 +19,8 @@
 #' percent_nonmissing(example_heart_1)
 
 percent_nonmissing <- function(data) {
-  time = hr = id = time_date = unique_days = total_days = NULL
-  rm(list = c('time', 'hr', 'id', 'time_date', 'unique_days', 'total_days'))
+  time = hr = id = time_date = time_min = unique_days = total_days = NULL
+  rm(list = c('time', 'hr', 'id', 'time_date', 'unique_days', 'total_days', 'time_min'))
   # Ensure necessary columns exist
   if (!all(c("id", "time") %in% colnames(data))) {
     stop("The dataset must contain 'id' and 'time' columns.")
@@ -29,7 +29,9 @@ percent_nonmissing <- function(data) {
   # Convert time column to Date-Time format
   data <- data |>
     dplyr::mutate(time = as.POSIXct(time, format = "%m/%d/%Y %I:%M:%S"),
-                  time_date = as.Date(time))  # Extract only the date
+                  time_date = as.Date(time),
+                  time_min = as.POSIXct(strftime(time, "%Y-%m-%d %H:%M:00")
+                  ))  # Extract only the date
 
   # Remove rows with missing HR values
   data <- dplyr::filter(data, !is.na(hr))
@@ -38,11 +40,12 @@ percent_nonmissing <- function(data) {
   active_data <- data |>
     dplyr::group_by(id) |>
     dplyr::summarize(
-      start_date = min(time),
-      end_date = max(time),
+      start_time = min(time),
+      end_time = max(time),
       unique_days = length(unique(format(time, "%m-%d-%Y"))),
       total_days = as.integer(difftime(max(time_date), min(time_date), units = "days")) + 1,
-      percent_nonmissing = (unique_days / total_days) * 100
+      percent_nonmissing = dplyr::n_distinct(time_min) / (as.integer(difftime(max(time_min), min(time_min),
+                                                                              units = "mins")) + 1) * 100
     )
 
   return(active_data)
